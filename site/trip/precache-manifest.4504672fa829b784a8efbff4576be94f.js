84911d43391a33d6de860ec37f7cf67304f09326
Stuart Buckingham <stu@astronomer.io>
2026-09-19 11:54:24 -0500

    import

---
+++ site/trip/precache-manifest.4504672fa829b784a8efbff4576be94f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f479cf040a89bb1a8814",
+    "url": "https://dwq8m6bqz6ura.cloudfront.net/trip/static/css/main.b19e0b5a.chunk.css"
+  },
+  {
+    "revision": "f479cf040a89bb1a8814",
+    "url": "https://dwq8m6bqz6ura.cloudfront.net/trip/static/js/main.f479cf04.chunk.js"
+  },
+  {
+    "revision": "4c67d12721f1cffa077e",
+    "url": "https://dwq8m6bqz6ura.cloudfront.net/trip/static/js/1.4c67d127.chunk.js"
+  },
+  {
+    "revision": "da7cf2cea3000d31f48a",
+    "url": "https://dwq8m6bqz6ura.cloudfront.net/trip/static/js/runtime~main.da7cf2ce.js"
+  },
+  {
+    "revision": "d5b7ec8b127d8bf2627a30f5f925a2f6",
+    "url": "https://dwq8m6bqz6ura.cloudfront.net/trip/index.html"
+  }
+];
---
+++ site/trip/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://dwq8m6bqz6ura.cloudfront.net/trip/precache-manifest.4504672fa829b784a8efbff4576be94f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://dwq8m6bqz6ura.cloudfront.net/trip/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
